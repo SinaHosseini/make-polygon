0f321a40f9279c6d4e38d15e74f3d87f01a23478
seyed sina hosseini vaez <122821690+SinaHosseini@users.noreply.github.com>
2023-02-27 21:48:22 +0330

    Add files via upload

---
+++ make turtle.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.bgcolor("black")
+
+colors = ["red", "blue", "purple", "green", "yellow", "orange"]
+
+p = turtle.Pen()
+
+i = 0
+while i < 270:
+    for j in range(len(colors)):
+        p.width(i / 75 + 1)
+        p.pencolor(colors[j])
+        p.forward(i)
+        p.left(59)
+        i += 1
+
+
+turtle.done()
